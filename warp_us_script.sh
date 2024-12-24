b8649f5a3c17fb6fc6e2fcf34800272e2b458123
he1ss <voorty91@gmail.com>
2024-12-24 13:00:44 +0300

    Update warp_us_script.sh

--- warp_us_script.sh
+++ warp_us_script.sh
@@ -1,37 +1,57 @@
 #!/bin/bash
 
-# Скрипт для генерации конфигурации Warp с сервером в США
+clear
+mkdir -p ~/.cloudshell && touch ~/.cloudshell/no-apt-get-warning # Для Google Cloud Shell, но лучше там не выполнять
+echo "Установка зависимостей..."
+apt update -y && apt install sudo -y # Для Aeza Terminator, там sudo не установлен по умолчанию
+sudo apt-get update -y --fix-missing && sudo apt-get install wireguard-tools jq wget -y --fix-missing # Update второй раз, если sudo установлен и обязателен (в строке выше не сработал)
 
-# Задаем IP-адрес сервера в США (например, Cloudflare)
-server="198.41.200.233"  # Пример IP Cloudflare в США
+priv="${1:-$(wg genkey)}"
+pub="${2:-$(echo "${priv}" | wg pubkey)}"
+api="https://api.cloudflareclient.com/v0i1909051800"
+ins() { curl -s -H 'user-agent:' -H 'content-type: application/json' -X "$1" "${api}/$2" "${@:3}"; }
+sec() { ins "$1" "$2" -H "authorization: Bearer $3" "${@:4}"; }
+response=$(ins POST "reg" -d "{\"install_id\":\"\",\"tos\":\"$(date -u +%FT%T.000Z)\",\"key\":\"${pub}\",\"fcm_token\":\"\",\"type\":\"ios\",\"locale\":\"en_US\"}")
 
-# Генерация ключей
-generate_keys() {
-    private_key=$(wg genkey)
-    public_key=$(echo "$private_key" | wg pubkey)
-    echo "Приватный ключ: $private_key"
-    echo "Публичный ключ: $public_key"
+clear
+id=$(echo "$response" | jq -r '.result.id')
+token=$(echo "$response" | jq -r '.result.token')
+response=$(sec PATCH "reg/${id}" "$token" -d '{"warp_enabled":true}')
+peer_pub=$(echo "$response" | jq -r '.result.config.peers[0].public_key')
+peer_endpoint=$(echo "$response" | jq -r '.result.config.peers[0].endpoint.host')
+client_ipv4=$(echo "$response" | jq -r '.result.config.interface.addresses.v4')
+client_ipv6=$(echo "$response" | jq -r '.result.config.interface.addresses.v6')
+
+reserved64=$(echo "$response" | jq -r '.result.config.client_id')
+reservedHex=$(echo "$reserved64" | base64 -d | hexdump -v -e '/1 "%02x\n"')
+reservedDec=$(printf '%s\n' "${reservedHex}" | while read -r hex; do printf "%d, " "0x${hex}"; done)
+reservedDec="[${reservedDec%, }]"
+reservedHex=$(echo "${reservedHex}" | awk 'BEGIN { ORS=""; print "0x" } { print }')
+
+conf=$(cat <<-EOM
+{
+"mtu": 1280,
+"reserved": "${reserved64}",
+"private_key": "${priv}",
+"type": "wireguard",
+"local_address": ["${client_ipv4}/24", "${client_ipv6}/128"],
+"peer_public_key": "${peer_pub}",
+"server": "104.16.172.73",  # Заменил на IP Cloudflare с локацией в США
+"server_port": 51820
 }
+EOM
+)
 
-# Генерация конфигурации для клиента WireGuard
-generate_config() {
-    cat <<EOL > wg0.conf
-[Interface]
-PrivateKey = $private_key
-Address = 10.0.0.2/24
-DNS = 1.1.1.1
-
-[Peer]
-PublicKey = $public_key
-Endpoint = $server:51820
-AllowedIPs = 0.0.0.0/0, ::/0
-PersistentKeepalive = 25
-EOL
-    echo "Конфигурация WireGuard сохранена в файл wg0.conf"
-}
-
-# Запуск скрипта
-generate_keys
-generate_config
-
-echo "Скрипт завершен. Конфигурация готова!"
+conf_base64=$(echo -n "${conf}" | base64 -w 0)
+echo -e "\n\n\n"
+[ -t 1 ] && echo "########## НАЧАЛО КОНФИГА ##########"
+echo "${conf}"
+[ -t 1 ] && echo "########### КОНЕЦ КОНФИГА ###########"
+echo "reserved в числах:"
+echo "\"reserved\": \"${reservedDec}\","
+echo -e "\n"
+echo "Иногда конфиг сверху не полный или отсутствует, поэтому лучше скачивайте по ссылке:"
+echo -e "\n"
+echo "https://immalware.github.io/downloader.html?filename=WARP.conf&content=${conf_base64}"
+echo -e "\n"
+echo "Что-то не получилось? Есть вопросы? Пишите в чат: https://t.me/vpn_1_1_1_1_warp"
